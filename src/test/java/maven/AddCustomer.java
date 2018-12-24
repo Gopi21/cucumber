ee29a6d569e35ac00eacd0c66f20cb2cfeaca1ee
Gopi21 <gopigop84@gmail.com>
2018-12-24 11:01:12 +0530

    added new process

--- src/test/java/maven/AddCustomer.java
+++ src/test/java/maven/AddCustomer.java
@@ -5,8 +5,11 @@ import java.util.Map;
 
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -29,28 +32,39 @@ public class AddCustomer {
 		driver.findElement(By.xpath("(//a[@href='addcustomer.php'])[1]")).click();
 	}
 
-	@When("the user enter the customer details")
+	@When("the user enter the customer details and clicks the submit button")
 	public void the_user_enter_the_customer_details(DataTable dataTable) {
-	//	List<String> list = dataTable.asList();
-//		driver.findElement(By.xpath("//label[text()='Done']")).click();
-//		driver.findElement(By.id("fname")).sendKeys(list.get(1));
-	//	driver.findElement(By.id("lname")).sendKeys(list.get(2));
-//		driver.findElement(By.id("email")).sendKeys(list.get(3));
-	//	driver.findElement(By.name("addr")).sendKeys(list.get(4));
-//		driver.findElement(By.id("telephoneno")).sendKeys(list.get(5));
-		
-	Map<String, String> ma = dataTable.asMap(String.class, String.class);
+		List<Map<String, String>> ma = dataTable.asMaps(String.class, String.class);
+		driver.findElement(By.xpath("//label[text()='Done']")).click();
+		driver.findElement(By.id("fname")).sendKeys(ma.get(1).get("fname"));
+		driver.findElement(By.id("lname")).sendKeys(ma.get(1).get("lname"));
+		driver.findElement(By.id("email")).sendKeys(ma.get(1).get("email"));
+		driver.findElement(By.name("addr")).sendKeys(ma.get(1).get("addr"));
+		driver.findElement(By.id("telephoneno")).sendKeys(ma.get(1).get("telephoneno"));
+
 	}
 
-	@When("The user clicks the submit button")
-	public void the_user_clicks_the_submit_button() {
+	@When("The user navigates to home page and cliks on Add Tariff Plan to Customer")
+	public void the_user_clicks_the_submit_button() throws Throwable {
 		driver.findElement(By.name("submit")).click();
+		Thread.sleep(1000);
+		String text = driver.findElement(By.xpath("//div[@class='table-wrapper']//h3")).getText();
+		driver.findElement(By.xpath("//a[@class='button']")).click();
+		driver.findElement(By.xpath("//div[@class='flex-item left']//a[text()='Add Tariff Plan to Customer']")).click();
+		driver.findElement(By.xpath("//input[@name='customer_id']")).sendKeys(text);
+		driver.findElement(By.xpath("//input[@name='submit']")).click();
+		WebElement radio = driver.findElement(By.xpath("//label[@for='sele']"));
+		radio.isSelected();
+		driver.findElement(By.xpath("//input[@name='submit']")).click();
+
 	}
 
 	@Then("The user should see a customer id generated")
 	public void the_user_should_see_a_customer_id_generated() {
-		
-		Assert.assertEquals("Please Note Down Your CustomerID", driver.findElement(By.xpath("//table[@class='alt access']//tr[2]")).getText());
+
+		Assert.assertEquals("Please Note Down Your CustomerID",
+				driver.findElement(By.xpath("//table[@class='alt access']//tr[2]")).getText());
+
 	}
 
 }
--- target/test-classes/Feature/AddCustomer.feature
+++ target/test-classes/Feature/AddCustomer.feature
@@ -1,12 +1,15 @@
 #Author: your.email@your.domain.com
-
 Feature: to test Add customer feature
 
   @tag1
   Scenario: to verify the customer ID with valid customer details
     Given The user is in guru telecome home page
     And The customer navigates to add customer page
-    When the user enter the customer details
-     | Done  | testfname | testlname | gopigrtyuio@gmail.com | address | 8122631285 |
-    And The user clicks the submit button
+    When the user enter the customer details and clicks the submit button
+      | backround | fname     | lname     | email                 | addr    | telephoneno |
+      | Done      | testfname | testlname | gopigrtyuio@gmail.com | address |  8122631285 |
+      | Done      | email     | gopi      | gopigrtyuio@gmail.com | address |  8122631285 |
+      | Done      | testfname | testlname | gopigrtyuio@gmail.com | address |  8122631285 |
+      | Done      | testfname | testlname | gopigrtyuio@gmail.com | address |  8122631285 |
+    And The user navigates to home page and cliks on Add Tariff Plan to Customer
     Then The user should see a customer id generated
